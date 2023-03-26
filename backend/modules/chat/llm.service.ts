45fd870c7860ee576d991b515506941fc255616c
Niklas <potentialforanything@gmail.com>
2023-03-26 13:43:38 +0200

    Added comments

--- backend/modules/chat/llm.service.ts
+++ backend/modules/chat/llm.service.ts
@@ -2,15 +2,19 @@ import { Injectable, Inject, OnModuleInit } from '@nestjs/common';
 import { OpenAI } from "langchain/llms";
 import { ChatOpenAI } from "langchain/chat_models";
 import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
-import { ConversationChain } from 'langchain/chains';
+import { ConversationChain, LLMChain } from 'langchain/chains';
 import { BufferMemory } from 'langchain/memory';
 import { ChatPromptTemplate, SystemMessagePromptTemplate, MessagesPlaceholder, HumanMessagePromptTemplate } from 'langchain/prompts';
+import { Calculator, SerpAPI } from 'langchain/tools';
+import { Agent, AgentExecutor, ChatAgent, initializeAgentExecutor, ZeroShotAgent } from 'langchain/agents';
 
 // const chat = new ChatOpenAI({ temperature: 0 });
 
 @Injectable()
 export class LlmService implements OnModuleInit {
   private conversationChain: ConversationChain | null = null;
+  private agentExecutor?: AgentExecutor | null = null;
+  private agent?: Agent | null = null;
   public isInitialized: boolean = false;
 
   constructor(
@@ -28,12 +32,36 @@ export class LlmService implements OnModuleInit {
       new MessagesPlaceholder("history"),
       HumanMessagePromptTemplate.fromTemplate("{input}"),
     ]);
-    
+
     this.conversationChain = new ConversationChain({
       memory: new BufferMemory({ returnMessages: true, memoryKey: "history" }),
       prompt: chatPrompt,
       llm: this.chat,
     });
+
+    //// Should make some kind of agent here eventually... so it can use other tools as well.
+    // this.agent = new ZeroShotAgent({llmChain: this.conversationChain, allowedTools: ["search", "calculator"]})
+    // this.agentExecutor = AgentExecutor.fromAgentAndTools({agent: this.agent, tools: tools});
+
+    // // Define the list of tools the agent can use
+    // const tools = [new SerpAPI("")];
+    // // const agent = ChatAgent.fromLLMAndTools(this.chat, tools);
+    // // this.agentExecutor = AgentExecutor.fromAgentAndTools({ 
+    // //   agent, 
+    // //   tools, 
+    // //   prompt: chatPrompt,
+    // //   memory: new BufferMemory({ returnMessages: true, memoryKey: "history" })});
+    // this.agentExecutor = await initializeAgentExecutor(
+    //   tools,
+    //   this.chat,
+    //   "chat-conversational-react-description",
+    //   true
+    // );
+    // this.agentExecutor.memory = new BufferMemory({
+    //   returnMessages: true,
+    //   memoryKey: "chat_history",
+    //   inputKey: "input",
+    // });
 
     this.isInitialized = true;
   }
@@ -42,7 +70,7 @@ export class LlmService implements OnModuleInit {
     return await this.model.call(prompt)
   }
 
-  async chain(prompt: string) {
-    return await this.conversationChain?.call({input: prompt})
+  async chain(input: string) {
+    return await this.conversationChain?.call({input})
   }  
 }
