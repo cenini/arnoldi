777b29aa966499c08353a3a38df5f19b7685f76a
Niklas <potentialforanything@gmail.com>
2023-04-23 20:40:41 +0200

    Add to settings component

--- frontend/src/routes/settings/settings.svelte
+++ frontend/src/routes/settings/settings.svelte
@@ -1,23 +1,61 @@
-<script lang="ts">
+<script>
 	import { signIn, signOut } from '@auth/sveltekit/client';
 	import { page } from '$app/stores';
 </script>
 
-<div>
-	<p>
-		{#if Object.keys($page.data.session || {}).length}
+<style>
+	.github-login-container {
+			display: flex;
+			align-items: center;
+			font-family: Arial, sans-serif;
+	}
+	.avatar {
+			width: 40px;
+			height: 40px;
+			border-radius: 50%;
+			background-position: center;
+			background-repeat: no-repeat;
+			background-size: cover;
+			margin-right: 10px;
+	}
+	.signedInText {
+			margin-right: 10px;
+	}
+	.signedInText strong {
+			display: block;
+	}
+	.github-login-button {
+			display: flex;
+			align-items: center;
+			background-color: #24292e;
+			color: white;
+			padding: 5px 10px;
+			border-radius: 4px;
+			border: none;
+			cursor: pointer;
+			font-size: 14px;
+	}
+	.github-login-button img {
+			width: 20px;
+			margin-right: 5px;
+	}
+</style>
+
+<div class="github-login-container">
+	{#if Object.keys($page.data.session || {}).length}
 			{#if $page.data.session.user.image}
-				<span style="background-image: url('{$page.data.session.user.image}')" class="avatar" />
+					<span style="background-image: url('{$page.data.session.user.image}')" class="avatar" />
 			{/if}
 			<span class="signedInText">
-				<small>Signed in as</small><br />
-				<strong>{$page.data.session.user.email || $page.data.session.user.name}</strong>
+					<small>Signed in as</small><br />
+					<strong>{$page.data.session.user.email || $page.data.session.user.name}</strong>
 			</span>
 			<button on:click={() => signOut()} class="button">Sign out</button>
-			<button on:click={() => console.log($page.data.session)}>Log session</button>
-		{:else}
-			<span class="notSignedInText">You are not signed in</span>
-			<button on:click={() => signIn('github')}>Sign In with GitHub</button>
-		{/if}
-	</p>
+			<!-- <button on:click={() => console.log($page.data.session)}>Log session</button> -->
+	{:else}
+			<button on:click={() => signIn('github')} class="github-login-button">
+					<img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub Icon" />
+					Sign In with GitHub
+			</button>
+	{/if}
 </div>
