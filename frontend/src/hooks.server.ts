7aca05c230f1ac09afe3d808a921eb4061aeee5d
Niklas <potentialforanything@gmail.com>
2023-04-23 20:44:57 +0200

    Remove references to google, will add later

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,16 +1,15 @@
 /* eslint-disable */
 // @ts-nocheck
 // See issue https://github.com/nextauthjs/next-auth/issues/6174
-import { SvelteGoogleAuthHook } from 'svelte-google-auth/server';
-import type { Handle } from '@sveltejs/kit';
+// import { SvelteGoogleAuthHook } from 'svelte-google-auth/server';
+// import type { Handle } from '@sveltejs/kit';
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/core/providers/github"
 import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
 
 // Import client credentials from json file
-import client_secret from '../google_client_secret.json';
-
-const auth = new SvelteGoogleAuthHook(client_secret.web);
+// import client_secret from '../google_client_secret.json';
+// const auth = new SvelteGoogleAuthHook(client_secret.web);
 
 // export const handle = async ({ event, resolve }) => {
 //   return SvelteKitAuth({
