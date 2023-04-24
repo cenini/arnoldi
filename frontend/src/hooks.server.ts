31d2b34ccb2d619421e88ef8b93566243a423796
Niklas <potentialforanything@gmail.com>
2023-04-24 19:32:49 +0200

    Add next public base url

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -21,3 +21,4 @@ import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
 export const handle = SvelteKitAuth({
   providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
 });
+
