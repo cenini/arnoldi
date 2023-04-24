c13026e333c0212d3d1604d70f9cf030395d3f33
Niklas <potentialforanything@gmail.com>
2023-04-24 21:51:24 +0200

    Remove custom redirect callback

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -22,19 +22,19 @@ export const handle = SvelteKitAuth({
   // trustHost: true,
   providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
   secret: AUTH_SECRET,
-  callbacks: {
-    async redirect(params: { url: string }) {
-      const { url } = params
+  // callbacks: {
+  //   async redirect(params: { url: string }) {
+  //     const { url } = params
 
-      // url is just a path, e.g.: /videos/pets
-      if (!url.startsWith('http')) return url
+  //     // url is just a path, e.g.: /videos/pets
+  //     if (!url.startsWith('http')) return url
 
-      // If we have a callback use only its relative path
-      const callbackUrl = new URL(url).searchParams.get('callbackUrl')
-      if (!callbackUrl) return url
+  //     // If we have a callback use only its relative path
+  //     const callbackUrl = new URL(url).searchParams.get('callbackUrl')
+  //     if (!callbackUrl) return url
 
-      return new URL(callbackUrl as string).pathname
-    },
-  }
+  //     return new URL(callbackUrl as string).pathname
+  //   },
+  // }
 });
 
