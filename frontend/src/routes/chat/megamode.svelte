262da9a46cbf9977ecefd27a152ddfa846013cb7
Niklas <potentialforanything@gmail.com>
2023-05-09 22:37:30 +0200

    Fix megamode not staying with a store

--- frontend/src/routes/chat/megamode.svelte
+++ frontend/src/routes/chat/megamode.svelte
@@ -2,19 +2,27 @@
 
   import { onMount } from 'svelte'
   import { themeChange } from 'theme-change'
+  import { megaMode } from '../../stores';
 
-  let megaMode: boolean = false;
+  let megaModeValue: boolean;
 
   onMount(() => {
     themeChange(false)
+    megaMode.subscribe(value => {
+      megaModeValue = value
+    })
   })
 
   function isDisabled() {
     return false;
   }
 
+  function updateMegaMode() {
+    megaMode.update(currentValue => !currentValue);
+  }
+
 </script>
 
 <div class="tooltip" data-tip="Mega-mode">
-  <input bind:checked={megaMode} type="checkbox" data-toggle-theme="valentine,coffee" class="toggle toggle-secondary" data-act-class="ACTIVECLASS" disabled='{isDisabled()}' />
+  <input checked={megaModeValue} on:click={(e) => updateMegaMode()} type="checkbox" data-toggle-theme="coffee,valentine" class="toggle toggle-secondary" data-act-class="ACTIVECLASS" disabled='{isDisabled()}' />
 </div>
