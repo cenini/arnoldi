3da353f6bd93c34981081f8927ca7cfdb35eb370
Niklas Nilsson <nini@netlight.com>
2023-08-25 11:20:49 +0200

    Improve error handling in controller

--- backend/src/modules/chat/chat.controller.ts
+++ backend/src/modules/chat/chat.controller.ts
@@ -1,4 +1,4 @@
-import { Inject, Controller, Body, Get, Post} from '@nestjs/common';
+import { Inject, Controller, Body, Get, Post, HttpException, InternalServerErrorException, HttpStatus, Logger} from '@nestjs/common';
 import { LlmService } from './llm.service.js';
 import { PromptDto } from './prompt.dto.js';
 import { MessageDto, SessionDto } from './session.dto.js';
@@ -13,11 +13,14 @@ function delay(ms: number) {
 export class ChatController {
   constructor(
     @Inject(LlmService) private readonly llmService: LlmService,
-    @Inject("SessionCollection") private readonly sessionCollection: Collection<Session>
+    @Inject("SessionCollection") private readonly sessionCollection: Collection<Session>,
     ) {}
+
+  private readonly logger = new Logger(ChatController.name);
 
   @Get()
   async index () {
+    this.logger.log("Hello!")
     return {
       message: 'Hello Coach!',
       date: new Date(),
@@ -25,13 +28,21 @@ export class ChatController {
   }
 
   @Post()
-  // async prompt(@Body() message: MessageDto) {
   async prompt(@Body() sessionDto: SessionDto) {
-    const session = SessionDto.toObject(sessionDto);
+    let session: Session
+    try {
+      session = SessionDto.toObject(sessionDto);      
+    } catch (e) {
+      throw new HttpException('Could not create session from request', HttpStatus.BAD_REQUEST)
+    }
     const filter = { Id: session.Id };
-    await this.sessionCollection.updateMany(filter, { $set:session }, { upsert: true });
-    let response = await this.llmService.chain(session);
-    return { message: response.replace(new RegExp("^(Arnold(?:\\sSchwarzenegger)?\\:)", "i"), "") };
+    try {
+      await this.sessionCollection.updateMany(filter, { $set:session }, { upsert: true });
+      let response = await this.llmService.chain(session);      
+      return { message: response.replace(new RegExp("^(Arnold(?:\\sSchwarzenegger)?\\:)", "i"), "") };
+    } catch (e) {
+      throw new InternalServerErrorException()
+    }
   }
 
   @Post("endsession")
